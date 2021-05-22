4ba4aa5c528bc4d5e4558ad99094731b9f496afd
peter <ptmartinezsound@gmail.com>
2021-05-22 14:38:32 -0500

    added homepage handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
+module.exports = router;
+
+
+
+// router.get('/', (req, res) => {
+//     res.render('homepage', {
+//         id: 1,
+//         title: 'TESTING STUFF',
+//         created_at: new Date(),
+//         comments: [{}, {}],
+//         user: {
+//             username: 'test_user'
+//         }
+//     });
+// });

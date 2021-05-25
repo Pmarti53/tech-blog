2a6e93aaec4876a9c87d5304814b492656e543d0
peter <ptmartinezsound@gmail.com>
2021-05-25 10:23:32 -0500

    creating session

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -6,7 +6,7 @@ async function signupFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        await fetch('/api/users', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
@@ -14,9 +14,40 @@ async function signupFormHandler(event) {
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
-        }).then((response) => { console.log(response) })
+        });
+        if (response.ok) {
+            console.log('success');
+        } else {
+            alert(response.statusText);
+        }
     }
 }
 
+async function signupFormHandler(event) {
+    event.preventDefault();
 
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                email,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
